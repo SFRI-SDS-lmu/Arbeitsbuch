29fa4daee2128553e4e2d925f924c46187973f87
langstefan33 <86051105+langstefan33@users.noreply.github.com>
2025-04-24 12:18:36 +0200

    Add files via upload

--- code/loes2_9.R
+++ code/loes2_9.R
@@ -1,90 +1,87 @@
 ##############################################################
-# Lösung Aufgabe 2.9 im Lehrbuch
+# Lösung Aufgabe 2.9 
 ##############################################################
 
 library("tseries")
 library("moments")
 
-# Einlesen der Daten ab 1.8.2007 bis 31.7.2008 ("Mitte 2008")
-bmw <- get.hist.quote(start="2007-08-01", end="2008-07-31", 
+# Aufgabe a) 
+# Einlesen der Daten 
+
+# Zeitraum 1.8.2007 bis 31.7.2008
+bmw_vor <- get.hist.quote(start="2007-08-01", end="2008-07-31", 
                       instrument="bmw.de", quote="AdjClose")
-r.bmw <- diff(log(bmw$Adjusted))
+r.bmw_vor <- diff(log(bmw_vor$Adjusted))
 
-#
-# 1.Arithmetisches Mittel, Median, Standardabweichung
-print(mean(r.bmw))
-print(median(r.bmw))
-print(sd(r.bmw))
-
-# 2. Histogramme, Boxplot
-hist(r.bmw, breaks=10)
-hist(r.bmw, breaks=20)
-boxplot(r.bmw)
-
-# 3. 
-# Schiefe (Quantilskoeffizient) und Wölbung
-
-qc.bmw <- ( (quantile(r.bmw,prob=0.75)-median(r.bmw) ) -
-              (median(r.bmw) - quantile(r.bmw,prob=0.25)) ) /
-  (quantile(r.bmw,prob=0.75)-quantile(r.bmw,prob=0.25) )
-cbmw <- kurtosis(r.bmw) -3
-
-print(qc.bmw)
-print(cbmw)
-
-pdf("loes2_9_vor_hist1.pdf")
-par(cex=1.5)
-hist(r.bmw, breaks=10)
-dev.off()
-pdf("loes2_9_vor_hist2.pdf")
-par(cex=1.5)
-hist(r.bmw, breaks=20)
-dev.off()
-pdf("loes2_9_vor_box.pdf")
-par(cex=1.5)
-boxplot(r.bmw)
-dev.off()
-
-# 4. Daten ab August 2008 bis 31.12.2009
-bmw <- get.hist.quote(start="2008-08-01", end="2009-12-31", 
+# Zeitraum August 2008 bis 31.12.2009
+bmw_nach <- get.hist.quote(start="2008-08-01", end="2009-12-31", 
                       instrument="bmw.de", quote="AdjClose")
-r.bmw <- diff(log(bmw$Adjusted))
-
-#
-# 1b.Arithmetisches Mittel, Median, Standardabweichung
-print(mean(r.bmw))
-print(median(r.bmw))
-print(sd(r.bmw))
-
-# 2b. Histogramme, Boxplot
-hist(r.bmw, breaks=10)
-hist(r.bmw, breaks=20)
-boxplot(r.bmw)
-
-# 3b. 
-# Schiefe (Quantilskoeffizient) und Wölbung
-
-qc.bmw <- ( (quantile(r.bmw,prob=0.75)-median(r.bmw) ) -
-              (median(r.bmw) - quantile(r.bmw,prob=0.25)) ) /
-  (quantile(r.bmw,prob=0.75)-quantile(r.bmw,prob=0.25) )
-cbmw <- kurtosis(r.bmw) -3
-
-print(qc.bmw)
-print(cbmw)
-
-pdf("loes2_9_nach_hist1.pdf")
-par(cex=1.5)
-hist(r.bmw, breaks=10)
-dev.off()
-pdf("loes2_9_nach_hist2.pdf")
-par(cex=1.5)
-hist(r.bmw, breaks=20)
-dev.off()
-pdf("loes2_9_nach_box.pdf")
-par(cex=1.5)
-boxplot(r.bmw)
-dev.off()
+r.bmw_nach <- diff(log(bmw_nach$Adjusted))
 
 
+# Aufgabe b) 
+# Arithmetisches Mittel
+print(mean(r.bmw_vor))   # vorher
+print(mean(r.bmw_nach))  # nachher
 
+# Median 
+print(median(r.bmw_vor))  # vorher
+print(median(r.bmw_nach)) # nachher
+
+# Standardabweichung
+print(sd(r.bmw_vor))      # vorher
+print(sd(r.bmw_nach))     # nachher
+
+# Aufgabe c)
+# Histogramme vorher
+par(mfrow=c(2,2))
+# Histogramm mit Standardeinstellungen
+# Standard breaks=14   
+hist(r.bmw_vor,main="breaks=14 (Standard)")             
+summary(h)                        
+# Histogramm mit weniger Breaks als Standard
+hist(r.bmw_vor, breaks=10,main="breaks=10") 
+# Histogramm mit mehr Breaks als Standard
+hist(r.bmw_vor, breaks=20,main="breaks=20") 
+
+# Histogramme nachher
+par(mfrow=c(2,2))
+# Histogramm mit Standardeinstellungen
+# Standard breaks=16
+hist(r.bmw_nach,main="breaks=16 (Standard)")     
+summary(h)          
+# Histogramm mit weniger Breaks als Standard
+hist(r.bmw_nach, breaks=10,main="breaks=10") 
+# Histogramm mit mehr Breaks als Standard
+hist(r.bmw_nach, breaks=20,main="breaks=20") 
+
+# Histogramme vorher nachher in einer Grafik vergleichen
+# zur besseren Vergleichbarkeit verwende gleiche Skala auf der vertikalen Achse
+par(mfrow=c(1,2))
+hist(r.bmw_vor,xlim=c(-0.15,0.12))
+hist(r.bmw_nach,xlim=c(-0.15,0.12))
+
+# Alternative Darstellung: Boxplots 
+# zur besseren Vergleichbarkeit verwende gleiche Skala auf der horizontalen Achse
+par(mfrow=c(1,2))
+boxplot(r.bmw_vor,main="vorher",ylim=c(-0.15,0.12))    # vorher
+boxplot(r.bmw_nach,main="nachher",ylim=c(-0.15,0.12)) # nachher
+
+
+# Aufgabe d)
+# Schiefe (Quartilskoeffizient) und Wölbung
+# vorher
+qc.bmw_vor <- ( (quantile(r.bmw_vor,prob=0.75)-median(r.bmw_vor) ) -
+              (median(r.bmw_vor) - quantile(r.bmw_vor,prob=0.25)) ) /
+  (quantile(r.bmw_vor,prob=0.75)-quantile(r.bmw_vor,prob=0.25) )
+cbmw_vor <- kurtosis(r.bmw_vor) -3
+print(qc.bmw_vor)
+print(cbmw_vor)
+# nachher
+qc.bmw_nach <- ( (quantile(r.bmw_nach,prob=0.75)-median(r.bmw_nach) ) -
+                  (median(r.bmw_nach) - quantile(r.bmw_nach,prob=0.25)) ) /
+  (quantile(r.bmw_nach,prob=0.75)-quantile(r.bmw_nach,prob=0.25) )
+cbmw_nach <- kurtosis(r.bmw_nach) -3
+print(qc.bmw_nach)
+print(cbmw_nach)
 
