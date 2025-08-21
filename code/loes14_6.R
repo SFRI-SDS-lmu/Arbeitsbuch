8563f37f3e69e66cab64851e00cefc3f0af7f809
langstefan33 <86051105+langstefan33@users.noreply.github.com>
2025-08-21 13:16:41 +0200

    Add files via upload

---
+++ code/loes14_6.R
@@ -0,0 +1,102 @@
+################################################################################
+# Lösung Aufgabe 14.6
+################################################################################
+
+# Lösche alle möglicherweise vorhandenen Objekte im Arbeitsbereich
+rm(list = ls())
+
+
+# Daten einlesen
+daten <- read.table(file="ifo_zeitreihen.txt", header=TRUE, dec=".")
+
+
+# ########################### Bauhauptgewerbe ##################################
+
+# Umwandlung in ein Zeitreihenobjekt
+bau.series <- ts(daten$Bauhauptgewerbe_Kapazitaetsauslastung, 
+                 start = c(1991, 1), frequency=12)
+
+# Starre Saisonfigur
+zerlegung.ifo.bau <- stl(bau.series, s.window="periodic")
+# ACHTUNG: 
+# Falls Ausgabe auf Bildschirm gewünscht (anstelle Grafik im pdf Format),
+# dann nur den plot Befehl ausführen
+pdf("loes14_6_bau1.pdf")
+par(cex=1.5)
+plot(zerlegung.ifo.bau)
+par(cex=1)
+dev.off()
+
+
+# Flexible Saisonfigur
+zerlegung.ifo.bau.2 <- stl(bau.series, s.window=7)
+# ACHTUNG: 
+# Falls Ausgabe auf Bildschirm gewünscht (anstelle Grafik im pdf Format),
+# dann nur den plot Befehl ausführen
+pdf("loes14_6_bau2.pdf")
+par(cex=1.5)
+plot(zerlegung.ifo.bau.2)
+par(cex=1)
+dev.off()
+
+
+# Vergleich der Trendkomponenten
+# ACHTUNG: 
+# Falls Ausgabe auf Bildschirm gewünscht (anstelle Grafik im pdf Format),
+# dann nur den plot, lines und legend Befehl ausführen
+pdf("loes14_6_trendvergleich_bau.pdf")
+par(cex=1.5)
+plot(zerlegung.ifo.bau$time.series[,2], lty=1)
+lines(zerlegung.ifo.bau.2$time.series[,2], lty=2)
+legend(1992, 74, legend=c("starre Saison", "flexible Saison"), lty=1:2)
+par(cex=1)
+dev.off()
+
+
+# ########################## Geschäftsklimaindex ###############################
+
+# Umwandlung in ein Zeitreihenobjekt
+gki.series <- ts(daten$Gewerbliche_Wirtschaft_Gki, 
+                 start = c(1991, 1), frequency=12)
+
+# Starre Saisonfigur
+zerlegung.ifo.gki <- stl(gki.series, s.window="periodic")
+# ACHTUNG: 
+# Falls Ausgabe auf Bildschirm gewünscht (anstelle Grafik im pdf Format),
+# dann nur den plot Befehl ausführen
+pdf("loes14_6_gki1.pdf")
+par(cex=1.5)
+plot(zerlegung.ifo.gki)
+par(cex=1)
+dev.off()
+
+
+# Flexible Saisonfigur
+zerlegung.ifo.gki.2 <- stl(gki.series, s.window=7)
+# ACHTUNG: 
+# Falls Ausgabe auf Bildschirm gewünscht (anstelle Grafik im pdf Format),
+# dann nur den plot Befehl ausführen
+pdf("loes14_6_gki2.pdf")
+par(cex=1.5)
+plot(zerlegung.ifo.gki.2)
+par(cex=1)
+dev.off()
+
+
+# Vergleich der Trendkomponenten 
+# ACHTUNG: 
+# Falls Ausgabe auf Bildschirm gewünscht (anstelle Grafik im pdf Format),
+# dann nur den plot, lines und legend Befehl ausführen
+pdf("loes14_6_trendvergleich.pdf")
+par(cex=1.5)
+plot(zerlegung.ifo.gki$time.series[,2], lty=1,
+     ylab="Trendkomponente GKI", xlab="Zeit")
+lines(zerlegung.ifo.gki.2$time.series[,2], lty=2)
+legend(1992, 112, legend=c("starre Saison", "flexible Saison"), lty=1:2)
+par(cex=1)
+dev.off()
+
+
+
+
+
