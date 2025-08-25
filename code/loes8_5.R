4634ade5cb6ac742b794bc1ce2f953dbd3a43457
langstefan33 <86051105+langstefan33@users.noreply.github.com>
2025-08-25 17:32:36 +0200

    Add files via upload

--- code/loes8_5.R
+++ code/loes8_5.R
@@ -1,6 +1,9 @@
-#################################################
+################################################################################
 # Lösung Aufgabe 8.5
-#################################################
+################################################################################
+
+# Lösche alle möglicherweise vorhandenen Objekte im Arbeitsbereich
+rm(list = ls())
 
 # Funktion bivdichte.nv
 # Implmentiere zunächst eine allgemeine Funktion zur Berechnung der 
@@ -27,12 +30,12 @@ y <- seq(from=-3,to=3, by=0.2)
 z <- matrix(nrow=length(x), ncol=length(y), data=0)
 
 
-# Aufgabe a) und b)
+############################## AUFGABE a), b) ##################################
+
 # Visualisiere die vier Dichten mit Hilfe der R Funktionen
-# persp, contour und image
+# 'persp', 'contour' und 'image'
 
-
-# ERSTE DICHTE
+############################### ERSTE DICHTE ###################################
 mu1 <- 0
 mu2 <- 0
 sigma1 <- 1
@@ -48,34 +51,34 @@ for (i in 1:length(x)) {
 # Verwende alternativ die outer Funktion zur Vermeidung der Doppelschleife
 z <- outer(x, y, bivdichte.nv, mu1, mu2, sigma1, sigma2, rho)
 
-
-
 # Visualisiere die Dichte
+# Zu den Parametern theta, phi, shade usw. konsultiere man die ensprechende
+# Hilfe, z.B. mit 'help(persp)'
+
 # ACHTUNG: 
 # Falls Ausgabe auf Bildschirm gewünscht (anstelle Grafik im pdf Format),
-# dann nur die persp, contour und image Befehle ausführen
-
-# Zu den Parametern theta, phi, shade usw. konsultiere man die ensprechende
-# Hilfe, z.B. mit help(persp)
-
+# dann nur die 'persp', 'contour' und 'image' Befehle ausführen
 pdf("loes_8_5_persp_1.pdf")
 par(cex.axis=1.2)
 persp(x,y,z, ticktype="detailed", 
       theta=30, phi=15, shade=0.1, expand=0.5, zlim=c(0,0.2), r=10)
+par(cex.axis=1)
 dev.off()
 
 pdf("loes_8_5_contour_1.pdf")
 par(cex=1.2)
 contour(x,y,z, zlim=c(0,0.2))
+par(cex=1)
 dev.off()
 
 pdf("loes_8_5_image_1.pdf")
 par(cex=1.2)
 image(x,y,z)
+par(cex=1)
 dev.off()
 
 
-# ZWEITE DICHTE  
+############################### ZWEITE DICHTE ##################################
 mu1 <- 0
 mu2 <- 0
 sigma1 <- 1.5
@@ -93,20 +96,24 @@ pdf("loes_8_5_persp_2.pdf")
 par(cex.axis=1.2)
 persp(x,y,z, ticktype="detailed", 
       theta=30, phi=15, shade=0.1, expand=0.5, zlim=c(0,0.2), r=10)
+par(cex.axis=1)
 dev.off()
 
 pdf("loes_8_5_contour_2.pdf")
 par(cex=1.2)
 contour(x,y,z, zlim=c(0,0.2))
+par(cex=1)
 dev.off()
 
 pdf("loes_8_5_image_2.pdf")
 par(cex=1.2)
 image(x,y,z)
+par(cex=1)
 dev.off()
 
 
-# DRITTE DICHTE
+################################ DRITTE DICHTE #################################
+
 mu1 <- 0
 mu2 <- 0
 sigma1 <- 1
@@ -118,26 +125,29 @@ z <- outer(x, y, bivdichte.nv, mu1, mu2, sigma1, sigma2, rho)
 # Visualisiere die Dichte
 # ACHTUNG: 
 # Falls Ausgabe auf Bildschirm gewünscht (anstelle Grafik im pdf Format),
-# dann nur die persp, contour und image Befehle ausführen
+# dann nur die 'persp', 'contour' und 'image' Befehle ausführen
 
 pdf("loes_8_5_persp_3.pdf")
 par(cex.axis=1.2)
 persp(x,y,z, ticktype="detailed", 
       theta=30, phi=15, shade=0.1, expand=0.5, zlim=c(0,0.2), r=10)
+par(cex.axis=1)
 dev.off()
 
 pdf("loes_8_5_contour_3.pdf")
 par(cex=1.2)
 contour(x,y,z, zlim=c(0,0.2))
+par(cex=1)
 dev.off()
 
 pdf("loes_8_5_image_3.pdf")
 par(cex=1.2)
 image(x,y,z)
+par(cex=1)
 dev.off()
 
 
-# VIERTE DICHTE
+############################### VIERTE DICHTE ##################################
 mu1 <- 0
 mu2 <- 0
 sigma1 <- 1
@@ -149,21 +159,24 @@ z <- outer(x, y, bivdichte.nv, mu1, mu2, sigma1, sigma2, rho)
 # Visualisiere die Dichte
 # ACHTUNG: 
 # Falls Ausgabe auf Bildschirm gewünscht (anstelle Grafik im pdf Format),
-# dann nur die persp, contour und image Befehle ausführen
+# dann nur die 'persp', 'contour' und 'image' Befehle ausführen
 
 pdf("loes_8_5_persp_4.pdf")
 par(cex.axis=1.2)
 persp(x,y,z, ticktype="detailed", 
       theta=30, phi=15, shade=0.1, expand=0.5, zlim=c(0,0.2), r=10)
+par(cex.axis=1)
 dev.off()
 
 pdf("loes_8_5_contour_4.pdf")
 par(cex=1.2)
 contour(x,y,z, zlim=c(0,0.2))
+par(cex=1)
 dev.off()
 
 pdf("loes_8_5_image_4.pdf")
 par(cex=1.2)
 image(x,y,z)
+par(cex=1)
 dev.off()
 
